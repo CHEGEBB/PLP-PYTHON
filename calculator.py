7f09c3f9f7f21eeb661085b82ebe328e4041c1f0
CHEGEBB <chegephil24@gmail.com>
2025-03-13 09:50:22 +0300

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 # Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
 # Perform the operation based on the user's input and print the result.
 # Example: If a user inputs 10, 5, and +, your program should display 10 + 5 = 15.
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number😊: "))
+num2 = int(input("Enter second number🤗: "))
 operation = input("Enter operation (+, -, *, /): ")
 
 if operation == "+":
